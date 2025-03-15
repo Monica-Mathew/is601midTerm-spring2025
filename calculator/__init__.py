bea450688daca25b752e43a315bad248e7fd399b
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-03-15 12:15:07 -0400

    updated function name

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -8,23 +8,23 @@ from calculator.calculation import Calculation
 
 class Calculator:
     @staticmethod
-    def _perform_operaration(a:Decimal, b:Decimal, operation: Callable[[Decimal, Decimal], Decimal]): # internal method to class
+    def _perform_operaration_Store_Store(a:Decimal, b:Decimal, operation: Callable[[Decimal, Decimal], Decimal]): # internal method to class
         calculation = Calculation.create(a,b,operation)
         Calculations.add_calculation_to_history(calculation)
         return calculation.performOperation()
 
     @staticmethod
     def addOperation(a:Decimal,b:Decimal) -> Decimal:
-        return Calculator._perform_operaration(a,b,add)
+        return Calculator._perform_operaration_Store(a,b,add)
 
     @staticmethod
     def subtractOpertiona(a:Decimal,b:Decimal) -> Decimal:
-        return Calculator._perform_operaration(a,b,subtract)
+        return Calculator._perform_operaration_Store(a,b,subtract)
     
     @staticmethod
     def multiplyOperation(a:Decimal,b:Decimal) -> Decimal:
-        return Calculator._perform_operaration(a,b,multiply)
+        return Calculator._perform_operaration_Store(a,b,multiply)
     
     @staticmethod
     def divideOperation(a:Decimal,b:Decimal) -> Decimal:
-        return Calculator._perform_operaration(a,b,divide)
+        return Calculator._perform_operaration_Store(a,b,divide)
