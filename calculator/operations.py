7eccaa1ed7ea875d3f6a5489c2e5622a0a2c82a0
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-03-15 14:08:58 -0400

    updated operations file

--- calculator/operations.py
+++ calculator/operations.py
@@ -4,14 +4,14 @@ from decimal import Decimal
 def add(a: Decimal, b:Decimal) -> Decimal:
     '''Add operation'''
     return a + b
-def subtract(a: Decimal, b:Decimal) -> Decimal:
-    '''subtract operation'''
-    return a + b
-def multiply(a: Decimal, b:Decimal) -> Decimal:
-    '''multiply operation'''
-    return a + b
+def subtract(a: Decimal,b:Decimal) -> Decimal:
+    '''Subtract operation'''
+    return a-b
+def multiply(a: Decimal,b:Decimal) -> Decimal:
+    '''Multiply operation'''
+    return a*b
 def divide(a: Decimal,b:Decimal) -> Decimal:
-    '''divide operation'''
+    '''Divide operation'''
     if b ==0:
         raise ValueError("Cannot divide by zero - Exception")
     return a/b
