54433d0a3ad5b71a57e9c888ed474b720675c391
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-03-15 15:51:58 -0400

    Adding command line class to register a command, and execute

---
+++ app/commands/__init__.py
@@ -0,0 +1,27 @@
+'''Command line file'''
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    '''Command class method '''
+    @abstractmethod
+    def execute(self):
+        '''execute method'''
+        pass # pylint: disable=unnecessary-pass
+
+class CommandHandler:
+    '''Command Handler class'''
+    def __init__(self):
+        '''initalizing the commands'''
+        self.commands = {}
+
+    def register_command(self, command_name: str, command: Command):
+        '''registering the commands with command handler'''
+        self.commands[command_name] = command
+
+    def execute_command(self, command_name: str,num1, num2):
+        '''function to execute based on the command name'''
+        try:
+            command = self.commands[command_name]
+            command.execute(num1, num2)
+        except KeyError:
+            print(f"No such command: {command_name}")
