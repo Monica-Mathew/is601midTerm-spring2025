798adf094a1f084fe973a045cfa60d5b827b1fa0
Monica-Mathew <63261145+Monica-Mathew@users.noreply.github.com>
2025-03-15 23:05:00 -0400

    updated

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,40 +1,39 @@
 '''Calculations file'''
 from decimal import Decimal
-from typing import List
 import os
 import pandas as pd
 from calculator.calculation import Calculation
 
 class Calculations:
     '''Calculations class'''
-    history: List[Calculation] =[] # history object to store list of calculations.
+    # history: List[Calculation] =[] # history object to store list of calculations.
 
-    @classmethod # adding as a classmethod to bind to the class itself to have it shared across all instances of clas
-    def add_calculation_to_history(cls, calculation:Calculation):
-        '''add a new calculation to the history'''
-        cls.history.append(calculation)
+    # @classmethod # adding as a classmethod to bind to the class itself to have it shared across all instances of clas
+    # def add_calculation_to_history(cls, calculation:Calculation):
+    #     '''add a new calculation to the history'''
+    #     cls.history.append(calculation)
 
-    @classmethod
-    def get_history(cls):
-        '''get the history of calculation'''
-        return cls.history # we are accessing the variable through the method
+    # @classmethod
+    # def get_history(cls):
+    #     '''get the history of calculation'''
+    #     return cls.history # we are accessing the variable through the method
     
-    @classmethod
-    def clear_history(cls):
-        '''Clear the history of calculation'''
-        cls.history.clear()
+    # @classmethod
+    # def clear_history(cls):
+    #     '''Clear the history of calculation'''
+    #     cls.history.clear()
     
-    @classmethod
-    def get_latest(cls) -> Calculation:
-        '''gets latest calculation'''
-        if cls.history:
-            return cls.history[-1]
-        return None
+    # @classmethod
+    # def get_latest(cls) -> Calculation:
+    #     '''gets latest calculation'''
+    #     if cls.history:
+    #         return cls.history[-1]
+    #     return None
     
-    @classmethod
-    def find_by_operation(cls, operation_name: str) -> Calculation:
-        '''add a new calculation to the history'''
-        return [calc for calc in cls.history if calc.operation.__name__ == operation_name]
+    # @classmethod
+    # def find_by_operation(cls, operation_name: str) -> Calculation:
+    #     '''add a new calculation to the history'''
+    #     return [calc for calc in cls.history if calc.operation.__name__ == operation_name]
 
     historyPd: pd.DataFrame 
 
